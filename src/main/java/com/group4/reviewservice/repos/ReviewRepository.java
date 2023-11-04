f073094f38bfb76e24ed652f8f927f995ce0b0ab
Pavan Krishna <29790828+iamPavanKrishna@users.noreply.github.com>
2023-11-04 14:06:34 +0530

    minor changes: formatting document

--- src/main/java/com/group4/reviewservice/repos/ReviewRepository.java
+++ src/main/java/com/group4/reviewservice/repos/ReviewRepository.java
@@ -5,7 +5,6 @@ import com.group4.reviewservice.models.Review;
 import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
