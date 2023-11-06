614d85df51f35903aaebdefbb7d27d51737be011
Pavan Krishna <29790828+iamPavanKrishna@users.noreply.github.com>
2023-11-07 02:31:08 +0530

    schema changes

--- src/main/java/com/group4/reviewservice/requests/CreateReviewInput.java
+++ src/main/java/com/group4/reviewservice/requests/CreateReviewInput.java
@@ -1,19 +1,16 @@
 package com.group4.reviewservice.requests;
 
-
-import java.util.Date;
 import java.util.UUID;
 
 import com.group4.reviewservice.enums.AttacthmentTypeEnum;
 import com.group4.reviewservice.models.Review;
 
-public record CreateReviewInput(UUID userId, UUID serviceId, Integer stars, String text, String useful, String funny, String cool, AttacthmentTypeEnum attachmentTypeEnum, String attachmentUrl) {
+public record CreateReviewInput(UUID userId, UUID serviceId, String text, String useful, String funny, String cool, AttacthmentTypeEnum attachmentTypeEnum, String attachmentUrl) {
     public Review toReview() {
         Review review = new Review();
 
         review.setUserId(userId)
         .setServiceId(serviceId)
-        .setStars(stars)
         .setText(text)
         .setUseful(useful)
         .setFunny(funny)
--- src/main/java/com/group4/reviewservice/services/ReviewServiceImpl.java
+++ src/main/java/com/group4/reviewservice/services/ReviewServiceImpl.java
@@ -1,5 +1,8 @@
 package com.group4.reviewservice.services;
 
+import com.group4.reviewservice.dtos.NotificationRequestDto;
+import com.group4.reviewservice.dtos.NotificationResponseDto;
+import com.group4.reviewservice.exceptions.*;
 import com.group4.reviewservice.models.Review;
 import com.group4.reviewservice.repos.ReviewRepository;
 import java.util.ArrayList;
@@ -7,28 +10,41 @@ import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import com.group4.reviewservice.thirdPartyNotification.TPAServiceCall;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Service;
 
 @Service("ReviewService")
 @Primary
+@RequiredArgsConstructor
 public class ReviewServiceImpl implements ReviewService{
 
     private final ReviewRepository reviewRepository;
+    private final TPAServiceCall tpaServiceCall;
 
-    @Autowired
-    public ReviewServiceImpl(ReviewRepository reviewRepository) {
-        this.reviewRepository = reviewRepository;
+
+    @Override
+    public Review submitReview(Review review) 
+            throws NotFoundException, TPAServiceException, InternalServerException, BadRequestException {
+                Review review2 = reviewRepository.save(review);
+                NotificationRequestDto notificationRequestDto = new NotificationRequestDto();
+
+                notificationRequestDto.setService_id(review2.getServiceId().toString());
+                notificationRequestDto.setData("Successfully Integrated Review Service with Notification Service");
+                notificationRequestDto.setImage_url("https://logowik.com/content/uploads/images/mail-notification4831.jpg");
+                notificationRequestDto.setSubject("Review Service and Notification Service Integration");
+                notificationRequestDto.setType("alert");
+                notificationRequestDto.setChannel("email");
+
+                NotificationResponseDto notificationResponseDto = tpaServiceCall.sendnotification(notificationRequestDto);
+                System.out.println(notificationResponseDto);
+                return review2;
     }
 
     @Override
-    public Review create(Review review) {
-        return reviewRepository.save(review);
-    }
-
-    @Override
-    public List<Review> findAll() {
+    public List<Review> getAllReviews()   
+            throws NotFoundException, TPAServiceException, InternalServerException {
         List<Review> review = new ArrayList<>();
         reviewRepository.findAll().forEach(review::add);
 
@@ -36,23 +52,34 @@ public class ReviewServiceImpl implements ReviewService{
     }
 
     @Override
-    public List<Review> findAllByServiceId(UUID id){
-        return  reviewRepository.findAllByServiceId((id));
+    public List<Review> getAllReviewsByServiceId(UUID id)    
+    throws NotFoundException, TPAServiceException, InternalServerException{
+        return  reviewRepository.getAllReviewsByServiceId(id);
 
     }
 
     @Override
-    public Optional<Review> findById(String id) {
+    public List<Review> getAllReviewsByUserId(UUID id)    
+    throws NotFoundException, TPAServiceException, InternalServerException{
+        return  reviewRepository.getAllReviewsByUserId(id);
+
+    }
+
+    @Override
+    public Optional<Review> getReviewById(String id) 
+            throws NotFoundException, InternalServerException {
         return reviewRepository.findById(UUID.fromString(id));
     }
 
     @Override
-    public Review update(Review review) {
+    public Review updateReview(Review review)   
+            throws NotFoundException, InternalServerException, BadRequestException {
         return reviewRepository.save(review);
     }
 
     @Override
-    public void delete(String id) {
+    public void deleteReview(String id)  
+            throws NotFoundException, InternalServerException {
         reviewRepository.deleteById(UUID.fromString(id));
     }
 }
