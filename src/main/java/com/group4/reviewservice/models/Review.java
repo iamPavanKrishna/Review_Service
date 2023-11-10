839a5fcb46f4d8a7307c8c0f325cd8a4a3edc1f0
Pavan Krishna <29790828+iamPavanKrishna@users.noreply.github.com>
2023-11-10 05:31:07 +0530

    schema changes

--- src/main/java/com/group4/reviewservice/models/Review.java
+++ src/main/java/com/group4/reviewservice/models/Review.java
@@ -2,6 +2,11 @@ package com.group4.reviewservice.models;
 
 import com.group4.reviewservice.enums.AttacthmentTypeEnum;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
@@ -9,9 +14,18 @@ import org.hibernate.annotations.UpdateTimestamp;
 import java.util.Date;
 import java.util.UUID;
 
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 @Table(name = "reviews")
 @Entity
-public class Review extends BaseModel{
+public class Review{
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(nullable = false)
+    private UUID id;
 
     @Column(nullable = false)
     private UUID userId;
@@ -30,15 +44,6 @@ public class Review extends BaseModel{
     @Column(length = 200, nullable = false)
     private String text;
 
-    @Column(nullable = false)
-    private String useful;
-    
-    @Column(nullable = false)
-    private String funny;
-    
-    @Column(nullable = false)
-    private String cool;
-
     @Column(nullable = false, columnDefinition = "varchar(20) not null")
     @Enumerated(EnumType.STRING)
     private AttacthmentTypeEnum attacthmentTypeEnum;
@@ -46,120 +51,117 @@ public class Review extends BaseModel{
     @Column(nullable = false)
     private String attachmentUrl;
 
+    @Column(nullable = false)
+    private Long likes = 0L;  // Default value is 0
+
+    @Column(nullable = false)
+    private Long dislikes = 0L;  // Default value is 0
+
+    // Other methods...
+
+    public void incrementLikes() {
+        this.likes++;
+    }
+
+    public void incrementDislikes() {
+        this.dislikes++;
+    }
+
     //@Column(nullable = false)
     //private Draft draft;  // need to clarify this
 
-    //Getters
+    // //Getters
 
-    public UUID getUserId() {
-        return userId;
-    }
+    // public UUID getUserId() {
+    //     return userId;
+    // }
     
-    public UUID getServiceId() {
-        return serviceId;
-    }
+    // public UUID getServiceId() {
+    //     return serviceId;
+    // }
 
-    public Date getCreatedAt() {
-        return createdAt;
-    }
+    // public Date getCreatedAt() {
+    //     return createdAt;
+    // }
 
-    public Date getUpdatedAt() {
-        return updatedAt;
-    }
+    // public Date getUpdatedAt() {
+    //     return updatedAt;
+    // }
 
-    public String getText() {
-        return text;
-    }
+    // public String getText() {
+    //     return text;
+    // }
 
-    public String getUseful() {
-        return useful;
-    }
+    // public Long getLike() {
+    //     return like;
+    // }
 
-    public String getFunny() {
-        return funny;
-    }
+    // }
 
-    public String getCool() {
-        return cool;
-    }
+    // public AttacthmentTypeEnum getAttacthmentTypeEnum() {
+    //     return attacthmentTypeEnum;
+    // }
 
-    public AttacthmentTypeEnum getAttacthmentTypeEnum() {
-        return attacthmentTypeEnum;
-    }
+    // public String getAttachmentUrl() {
+    //     return attachmentUrl;
+    // }
 
-    public String getAttachmentUrl() {
-        return attachmentUrl;
-    }
+    // //Setters
 
-    //Setters
+    // public Review setUserId(UUID userId) {
+    //     this.userId = userId;
+    //     return this;
+    // }
 
-    public Review setUserId(UUID userId) {
-        this.userId = userId;
-        return this;
-    }
+    // public Review setServiceId(UUID serviceId) {
+    //     this.serviceId = serviceId;
+    //     return this;
+    // }
 
-    public Review setServiceId(UUID serviceId) {
-        this.serviceId = serviceId;
-        return this;
-    }
+    // public Review setCreatedAt(Date createdAt) {
+    //     this.createdAt = createdAt;
+    //     return this;
+    // }
 
-    public Review setCreatedAt(Date createdAt) {
-        this.createdAt = createdAt;
-        return this;
-    }
+    // public Review setUpdatedAt(Date updatedAt) {
+    //     this.updatedAt = updatedAt;
+    //     return this;
+    // }
 
-    public Review setUpdatedAt(Date updatedAt) {
-        this.updatedAt = updatedAt;
-        return this;
-    }
+    // public Review setText(String text) {
+    //     this.text = text;
+    //     return this;
+    // }
 
-    public Review setText(String text) {
-        this.text = text;
-        return this;
-    }
+    // public Review setUseful(String useful) {
+    //     this.like = like;
+    //     return this;
+    // }
 
-    public Review setUseful(String useful) {
-        this.useful = useful;
-        return this;
-    }
+    // public Review setAttacthmentTypeEnum(AttacthmentTypeEnum attacthmentTypeEnum) {
+    //     this.attacthmentTypeEnum = attacthmentTypeEnum;
+    //     return this;
+    // }
 
-    public Review setFunny(String funny) {
-        this.funny = funny;
-        return this;
-    }
+    // public Review setAttachmentUrl(String attachmentUrl) {
+    //     this.attachmentUrl = attachmentUrl;
+    //     return this;
+    // }
 
-    public Review setCool(String cool) {
-        this.cool = cool;
-        return this;
-    }
+    // //toString
 
-    public Review setAttacthmentTypeEnum(AttacthmentTypeEnum attacthmentTypeEnum) {
-        this.attacthmentTypeEnum = attacthmentTypeEnum;
-        return this;
-    }
-
-    public Review setAttachmentUrl(String attachmentUrl) {
-        this.attachmentUrl = attachmentUrl;
-        return this;
-    }
-
-    //toString
-
-    @Override
-    public String toString() {
-        return "Review{" +
-                "userId=" + userId +
-                ", serviceId=" + serviceId +
-                ", createdAt=" + createdAt +
-                ", updatedAt=" + updatedAt +
-                ", text='" + text + '\'' +
-                ", useful='" + useful + '\'' +
-                ", funny='" + funny + '\'' +
-                ", cool='" + cool + '\'' +
-                ", attacthmentTypeEnum=" + attacthmentTypeEnum +
-                ", attachmentUrl='" + attachmentUrl + '\'' +
-                '}';
-    }
+    // @Override
+    // public String toString() {
+    //     return "Review{" +
+    //             "userId=" + userId +
+    //             ", serviceId=" + serviceId +
+    //             ", createdAt=" + createdAt +
+    //             ", updatedAt=" + updatedAt +
+    //             ", text='" + text + '\'' +
+    //             ", attacthmentTypeEnum=" + attacthmentTypeEnum +
+    //             ", attachmentUrl='" + attachmentUrl + '\'' +
+    //             '}';
+    // }
     
 
 }
