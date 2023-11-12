ac943f2985bad8f0bfb14806e53b70420efb2eef
Pavan Krishna <29790828+iamPavanKrishna@users.noreply.github.com>
2023-11-12 15:00:20 +0530

    updated updateReview input

--- src/main/java/com/group4/reviewservice/dtos/requests/UpdateReviewInput.java
+++ src/main/java/com/group4/reviewservice/dtos/requests/UpdateReviewInput.java
@@ -6,13 +6,8 @@ import java.util.UUID;
 
 // This UpdateReviewInput is a record which acts as a Data Transfer Object for the input request body of the update Review API route
 public record UpdateReviewInput(
-    UUID userId, 
-    UUID serviceId, 
     String text, 
     AttachmentTypeEnum attachmentTypeEnum, 
-    String attachmentUrl,
-    int usefulCount,
-    int funnyCount,
-    int coolCount) {
+    String attachmentUrl) {
 
 }
